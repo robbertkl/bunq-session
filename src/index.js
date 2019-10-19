93add6684f7a66069f11e7b3f765f120c0ca3a93
Robbert Klarenbeek <robbertkl@renbeek.nl>
2019-10-19 16:22:06 +0200

    Fix login concurrency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import bunqHttp from 'bunq-http';
 import { rsa, pki } from 'node-forge';
+import { Mutex } from 'async-mutex';
 import { promisify } from 'util';
 import { name as packageName, version as packageVersion } from '../package.json';
 import MemoryStore from './store/MemoryStore.js';
@@ -45,108 +46,117 @@ export class BunqSession {
       },
     });
 
-    this.session = null;
+    this.mutex = new Mutex();
+    this.session = undefined;
   }
 
   async login() {
-    if (!this.http.defaults.bunq.clientPrivateKey) {
-      const clientPrivateKeyPem = await this.store.get('clientPrivateKey', async () => {
-        const keyPair = await generateKeyPair({ bits: 2048 });
-        return pki.privateKeyToPem(keyPair.privateKey);
-      });
-      this.http.defaults.bunq.clientPrivateKey = pki.privateKeyFromPem(clientPrivateKeyPem);
-    }
-
-    if (!this.http.defaults.bunq.serverPublicKey || !this.installationToken) {
-      const installation = await this.store.get('installation', async () => {
-        const response = await this.http.post(
-          'installation',
-          { client_public_key: pki.publicKeyToPem(this.http.defaults.bunq.clientPrivateKey) },
-          { bunq: { shouldSignRequest: false, shouldVerifyResponseSignature: false } }
-        );
-        return {
-          token: response.bunq.objectsByType.Token[0].token,
-          serverPublicKey: response.bunq.objectsByType.ServerPublicKey[0].server_public_key,
-        };
-      });
-      this.http.defaults.bunq.serverPublicKey = pki.publicKeyFromPem(installation.serverPublicKey);
-      this.installationToken = installation.token;
-    }
-
-    if (!this.deviceId) {
-      this.deviceId = await this.store.get('deviceId', async () => {
-        const response = await this.http.post(
-          'device-server',
-          { secret: this.apiKey, description: this.deviceDescription },
-          { bunq: { token: this.installationToken } }
-        );
-        return response.bunq.objectsByType.Id[0].id;
-      });
-    }
-
-    const createNewSession = async () => {
-      const response = await this.http.post(
-        'session-server',
-        { secret: this.apiKey },
-        { bunq: { token: this.installationToken } }
-      );
-      const userObject =
-        'UserPerson' in response.bunq.objectsByType
-          ? response.bunq.objectsByType.UserPerson[0]
-          : response.bunq.objectsByType.UserCompany[0];
-      return {
-        token: response.bunq.objectsByType.Token[0].token,
-        userId: userObject.id,
-        userType: userObject.__type, // eslint-disable-line no-underscore-dangle
-      };
-    };
-
-    if (this.persistSession) {
-      if (this.session) {
-        this.session = null;
-        this.session = await createNewSession();
-        await this.store.set('session', this.session);
-      } else {
-        this.session = await this.store.get('session', createNewSession);
+    let release;
+    try {
+      if (!this.http.defaults.bunq.clientPrivateKey) {
+        if (!release) release = await this.mutex.acquire();
+        if (!this.http.defaults.bunq.clientPrivateKey) {
+          const clientPrivateKeyPem = await this.store.get('clientPrivateKey', async () => {
+            const keyPair = await generateKeyPair({ bits: 2048 });
+            return pki.privateKeyToPem(keyPair.privateKey);
+          });
+          this.http.defaults.bunq.clientPrivateKey = pki.privateKeyFromPem(clientPrivateKeyPem);
+        }
       }
-    } else {
-      this.session = null;
-      this.session = await createNewSession();
+
+      if (!this.http.defaults.bunq.serverPublicKey || !this.installationToken) {
+        if (!release) release = await this.mutex.acquire();
+        if (!this.http.defaults.bunq.serverPublicKey || !this.installationToken) {
+          const installation = await this.store.get('installation', async () => {
+            const response = await this.http.post(
+              'installation',
+              { client_public_key: pki.publicKeyToPem(this.http.defaults.bunq.clientPrivateKey) },
+              { bunq: { shouldSignRequest: false, shouldVerifyResponseSignature: false } }
+            );
+            return {
+              token: response.bunq.objectsByType.Token[0].token,
+              serverPublicKey: response.bunq.objectsByType.ServerPublicKey[0].server_public_key,
+            };
+          });
+          this.http.defaults.bunq.serverPublicKey = pki.publicKeyFromPem(installation.serverPublicKey);
+          this.installationToken = installation.token;
+        }
+      }
+
+      if (!this.deviceId) {
+        if (!release) release = await this.mutex.acquire();
+        if (!this.deviceId) {
+          this.deviceId = await this.store.get('deviceId', async () => {
+            const response = await this.http.post(
+              'device-server',
+              { secret: this.apiKey, description: this.deviceDescription },
+              { bunq: { token: this.installationToken } }
+            );
+            return response.bunq.objectsByType.Id[0].id;
+          });
+        }
+      }
+
+      if (!this.session) {
+        if (!release) release = await this.mutex.acquire();
+        if (this.session === undefined && this.persistSession) {
+          this.session = await this.store.get('session');
+        }
+        if (!this.session) {
+          const response = await this.http.post(
+            'session-server',
+            { secret: this.apiKey },
+            { bunq: { token: this.installationToken } }
+          );
+          const userObject =
+            'UserPerson' in response.bunq.objectsByType
+              ? response.bunq.objectsByType.UserPerson[0]
+              : response.bunq.objectsByType.UserCompany[0];
+          this.session = {
+            token: response.bunq.objectsByType.Token[0].token,
+            userId: userObject.id,
+            userType: userObject.__type, // eslint-disable-line no-underscore-dangle
+          };
+          if (this.persistSession) {
+            await this.store.set('session', this.session);
+          }
+        }
+
+        this.http.defaults.bunq.token = this.session.token;
+      }
+    } finally {
+      if (release) release();
     }
-    this.http.defaults.bunq.token = this.session.token;
   }
 
   async getUserId() {
-    if (!this.session) await this.login();
+    await this.login();
     return this.session.userId;
   }
 
   async getUserType() {
-    if (!this.session) await this.login();
+    await this.login();
     return this.session.userType;
   }
 
   async request(method, resource, options = {}, data = null) {
-    if (!this.session) await this.login();
+    await this.login();
 
     options.method = method;
     options.url = resource;
     if (data) options.data = data;
 
-    let response;
     try {
-      response = await this.http.request(options);
+      return await this.http.request(options);
     } catch (error) {
-      if (error.response && error.response.status === 401) {
-        // Possibly expired session, retry once after creating a new session
+      if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+        // Possibly expired session, retry with fresh one
+        this.session = null;
         await this.login();
-        response = await this.http.request(options);
-      } else {
-        throw error;
+        return this.http.request(options);
       }
+      throw error;
     }
-
-    return response;
   }
 
   async delete(resource, options = {}) {
